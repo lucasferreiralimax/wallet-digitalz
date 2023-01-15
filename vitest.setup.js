87484ba70e8c377b26ca3c36ae34a4365a31b207
lucasferreiralimax <lucasferreiralimax@gmail.com>
2023-01-15 00:52:10 -0300

    test(unit): adjustment config i18n

--- vitest.setup.js
+++ vitest.setup.js
@@ -2,7 +2,10 @@ import { config } from '@vue/test-utils'
 import { vi } from 'vitest'
 
 import { createTestingPinia } from '@pinia/testing'
+import i18n from '@/i18n'
 
 config.global.plugins = [
   createTestingPinia({ createSpy: vi.fn() })
 ]
+
+config.global.plugins = [i18n()]
