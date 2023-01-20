85cf80caf5e26af74db2f2c72a56fac1d978d35c
lucasferreiralimax <lucasferreiralimax@gmail.com>
2023-01-19 22:17:05 -0300

    test(e2e): specs for home page texts content

--- cypress/e2e/specs/Home.cy.ts
+++ cypress/e2e/specs/Home.cy.ts
@@ -1,6 +1,20 @@
-describe('Test render home page', () => {
-  it('Render HeaderApp with title', () => {
+import pkg from '../../../package.json'
+
+const appVersion: string = pkg.version
+
+describe('Test render Home page pt-BR', () => {
+  it('Render title app', () => {
     cy.visit('/')
     cy.contains('h1', 'Wallet Digitalz')
+  })
+  it('Render texts initial', () => {
+    cy.contains('h2', 'Comece agora os seus registros')
+    cy.contains('p', 'Este aplicativo é para ajudar a gerenciar seu dinheiro, despesas e investimentos')
+  })
+  it('Render text footer', () => {
+    cy.contains('a', 'Wallet Digitalz — 2023')
+  })
+  it('Render version in footer', () => {
+    cy.contains('p', `Versão ${appVersion}`)
   })
 })
