79071b62a526b618b255ad14ea34a29d5b71f83d
lucasferreiralimax <lucasferreiralimax@gmail.com>
2023-02-03 22:40:59 -0300

    test(e2e): adjustment orde test

--- cypress/e2e/specs/RegisterBasic.cy.ts
+++ cypress/e2e/specs/RegisterBasic.cy.ts
@@ -3,6 +3,15 @@ import pkg from '../../../package.json'
 const appVersion: string = pkg.version
 
 describe('Test render register basic', () => {
+  it('Render a investiment basic', () => {
+    cy.get('[aria-label="New register"]').click()
+    cy.get('[aria-label="Select type register"]').type('Investimento', { force: true })
+    cy.get('.v-select__content .v-list-item:nth-of-type(1)').click({ force: true })
+    cy.get('[aria-label="Name of register"]').type('Open Source', { force: true })
+    cy.get('[aria-label="Value of register"]').type(1000000, { force: true })
+    cy.get('[aria-label="Description of register"]').type('Investiment in community open source', { force: true })
+    cy.get('[aria-label="Create register"]').click()
+  })
   it('Render a expense basic', () => {
     cy.visit('/')
     cy.get('[aria-label="New register"]').click()
@@ -11,15 +20,6 @@ describe('Test render register basic', () => {
     cy.get('[aria-label="Name of register"]').type('Freelance Vue', { force: true })
     cy.get('[aria-label="Value of register"]').type(10000, { force: true })
     cy.get('[aria-label="Description of register"]').type('Create a application with Vue', { force: true })
-    cy.get('[aria-label="Create register"]').click()
-  })
-  it('Render a investiment basic', () => {
-    cy.get('[aria-label="New register"]').click()
-    cy.get('[aria-label="Select type register"]').type('Investimento', { force: true })
-    cy.get('.v-select__content .v-list-item:nth-of-type(1)').click({ force: true })
-    cy.get('[aria-label="Name of register"]').type('Open Source', { force: true })
-    cy.get('[aria-label="Value of register"]').type(1000000, { force: true })
-    cy.get('[aria-label="Description of register"]').type('Investiment in community open source', { force: true })
     cy.get('[aria-label="Create register"]').click()
   })
   it('Render a investiment basic', () => {
