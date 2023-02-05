eaedb84ced91c3ee6b9c479a7f7c973b7414382b
lucasferreiralimax <lucasferreiralimax@gmail.com>
2023-02-05 04:44:23 -0300

    test(unit): add createVuetify() in setup tests

--- vitest.setup.js
+++ vitest.setup.js
@@ -2,10 +2,11 @@ import { config } from '@vue/test-utils'
 import { vi } from 'vitest'
 
 import { createTestingPinia } from '@pinia/testing'
+import { createVuetify } from "vuetify";
 import i18n from '@/i18n'
 
 config.global.plugins = [
-  createTestingPinia({ createSpy: vi.fn() })
+  createTestingPinia({ createSpy: vi.fn() }),
+  createVuetify(),
+  i18n()
 ]
-
-config.global.plugins = [i18n()]
