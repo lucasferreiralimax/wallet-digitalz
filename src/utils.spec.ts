ddb067829d889725e756bb40e42cced83fdaaf11
lucasferreiralimax <lucasferreiralimax@gmail.com>
2023-02-05 20:55:36 -0300

    test(unit): improvement validation parseMoney with params eye

--- src/utils.spec.ts
+++ src/utils.spec.ts
@@ -3,18 +3,36 @@ import { parseMoney } from './utils'
 
 describe('Basic render parseMoney function', () => {
   it('is parseMoney BRL', () => {
-    expect(parseMoney(1000, 'pt-BR')).toBe('R$ 1.000,00')
+    expect(parseMoney(1000, 'pt-BR', true)).toBe('R$ 1.000,00')
   })
   it('is parseMoney USD', () => {
-    expect(parseMoney(1000, 'en-US')).toBe('$1,000.00')
+    expect(parseMoney(1000, 'en-US', true)).toBe('$1,000.00')
   })
   it('is parseMoney EUR', () => {
-    expect(parseMoney(1000, 'es-ES')).toBe('1000,00 €')
+    expect(parseMoney(1000, 'es-ES', true)).toBe('1000,00 €')
   })
   it('is parseMoney RUB', () => {
-    expect(parseMoney(1000, 'ru-RU')).toBe('1 000,00 ₽')
+    expect(parseMoney(1000, 'ru-RU', true)).toBe('1 000,00 ₽')
   })
   it('is parseMoney CNY', () => {
-    expect(parseMoney(1000, 'zh-CN')).toBe('¥1,000.00')
+    expect(parseMoney(1000, 'zh-CN', true)).toBe('¥1,000.00')
+  })
+})
+
+describe('Basic render parseMoney with hidden value', () => {
+  it('is parseMoney BRL', () => {
+    expect(parseMoney(1000, 'pt-BR', false)).toBe('R$ ********')
+  })
+  it('is parseMoney USD', () => {
+    expect(parseMoney(1000, 'en-US', false)).toBe('$********')
+  })
+  it('is parseMoney EUR', () => {
+    expect(parseMoney(1000, 'es-ES', false)).toBe('******* €')
+  })
+  it('is parseMoney RUB', () => {
+    expect(parseMoney(1000, 'ru-RU', false)).toBe('* ****** ₽')
+  })
+  it('is parseMoney CNY', () => {
+    expect(parseMoney(1000, 'zh-CN', false)).toBe('¥********')
   })
 })
